ef4d91b05042fee3e55b95770d77c271a7d5a70d
Twitch1209 <39025598+Twitch1209@users.noreply.github.com>
2019-05-03 13:44:10 +0800

    add notes

--- mingshi/mingshi/pipelines.py
+++ mingshi/mingshi/pipelines.py
@@ -4,7 +4,7 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
-
+# use json to save data
 import json
 import codecs
 import sys
@@ -12,9 +12,11 @@ import sys
 
 class mingshiPipeline(object):
     def __init__(self):
+        # you can change Han.json
         self.file = codecs.open('Han.json', 'w', encoding='utf-8')
 
     def process_item(self, item, spider):
+        # change to determain json you like
         line = json.dumps(dict(item), ensure_ascii=False) + "\n"
 
         self.file.write(line)
